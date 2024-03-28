b1f245f3b7eceacab640ea3ad2b02bfe270d251e
Chaehwi Hwang <chaehwi.h@gmail.com>
2024-03-28 11:50:18 +0900

    240328 while, for

---
+++ 240328_while,for.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<windows.h>
+
+
+int main(void){
+	
+ 	/* int cnt = 1, dan = 2;
+	
+	while(cnt <= 10 && dan < 10){
+		printf("%d * %d = %d\n", dan, cnt, cnt * dan);
+		cnt += 1;
+		}
+		
+	*/
+	
+	// while
+	/*
+		while (i <= 10){
+			
+			sum = sum + i;
+			printf("i = %2d, sum = %3d\n", i, sum);
+			i += 1;
+			
+			Sleep(100); // ms 소문자(sleep)은 s 
+		}
+		
+	*/	
+	
+	// for
+	
+	int dan, i;
+	
+/*	for(dan = 2; dan <= 9; dan++){
+		for(i = 1; i <= 9; i++){
+			printf("%d * %d = %d\n", dan, i, dan * i);
+		}
+		printf("\n");
+	}
+*/
+	
+	int sum, j;
+	
+	for(j = 1; j <= 10; j++){
+		
+			printf("%d. 황채휘 / 2414340025\n", j);
+		}
+	 	
+	}
+
